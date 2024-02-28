dfd08e3635b08efdf3fe89a4a2a157db42f094c2
Krishna <ravichandrakrishna555@gmail.com>
2024-02-28 21:01:45 +0530

    Update

--- js/mj.js
+++ js/mj.js
@@ -1,1 +1,8 @@
-console.log("MJ loaded");
+
+function msgTriggered(event) {
+    if(!event.data) return;
+    let msgData = event.data;
+    console.log('Got', msgData);
+}
+
+window.addEventListener("message", msgTriggered);
