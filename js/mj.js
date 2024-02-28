1cd4bd48779ac49344b36d6dd1879bdf35604167
Krishna <ravichandrakrishna555@gmail.com>
2024-02-28 21:50:05 +0530

    Update

--- js/mj.js
+++ js/mj.js
@@ -1,16 +1,13 @@
-
 function msgTriggered(event) {
-    if(!event.data || !event.data.msgType) return;
-    let msgType = event.data.msgType;
+    if (!event.data || !event.data.msgType) return;
 
-
-    switch (msgType) {
+    switch (event.data.msgType) {
         case "getData":
             let obj = {
-                msgType: msgType,
-                msgKey: msgKey,
-                value: localStorage.getItem(event.data.msgKey)
-            }
+                msgType: event.data.msgType,
+                msgKey: event.data.msgKey,
+                value: localStorage.getItem(event.data.msgKey),
+            };
 
             event.source.postMessage(obj, "*");
             break;
@@ -20,7 +17,7 @@ function msgTriggered(event) {
         default:
             data = {};
     }
-    console.log('Got', msgData);
+    console.log("Got", msgData);
 }
 
 window.addEventListener("message", msgTriggered);
