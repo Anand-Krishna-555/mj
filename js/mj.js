0ef2ae6f498e3dc027f6ac88d9ca9652a6f8cb54
Krishna <ravichandrakrishna555@gmail.com>
2024-02-28 21:28:05 +0530

    Update

--- js/mj.js
+++ js/mj.js
@@ -1,7 +1,19 @@
 
 function msgTriggered(event) {
-    if(!event.data) return;
-    let msgData = event.data;
+    if(!event.data || !event.data.msgType) return;
+    let msgType = event.data.msgType;
+
+
+    switch (msgType) {
+        case "getData":
+            event.source.postMessage(localStorage.getItem(event.data.msgKey), "*");
+            break;
+        case "setData":
+            break;
+
+        default:
+            data = {};
+    }
     console.log('Got', msgData);
 }
 
